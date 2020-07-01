cc773f8321ab8061a9513270ba9b76bba658ca8a
Szymon Wrzos <szymon.wrzos@onet.pl>
2020-07-01 22:02:19 +0200

    More functionality do webapp.py

--- webapp.py
+++ webapp.py
@@ -1,1 +1,13 @@
-import flask
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/entry')
+def render() -> 'html':
+    return render_template('entry.html',
+                           tytul="Strona!")
+
+
+app.run()
