37b087563f2d59ae0088d46af784b76548da67d2
Szymon Wrzos <szymon.wrzos@onet.pl>
2020-07-03 21:49:53 +0200

    Updated function that logs form completion

--- webapp.py
+++ webapp.py
@@ -1,5 +1,5 @@
-from flask import Flask, render_template, request
-
+from flask import Flask, render_template, request, redirect, escape
+import datetime
 app = Flask(__name__)
 
 
@@ -9,8 +9,21 @@ def render() -> 'html':
     return render_template('entry.html',
                            tytul="Strona!")
 
-@app.route('/log_to_file')
-def log_to_file() -> 'file':
-    pass
-    #with open('data.log','a') as file
+
+@app.route('/log_to_file', methods=['POST'])
+def log_to_file() -> 'redirect':
+    lista: list = []
+    with open('data.log', 'a') as log_file:
+        string_to_inject = ""
+        print(datetime.datetime.now() ,file=log_file, end=" | ")
+        print("[", end='', file=log_file)
+        for i in request.form:
+            string_to_inject += escape(request.form[i]) + ","
+        string_to_inject = string_to_inject[:-1]
+        print(string_to_inject, file=log_file, end="")
+        print("]", end='', file=log_file)
+        print(file=log_file)
+    return redirect('/')
+
+
 app.run(debug=True)
